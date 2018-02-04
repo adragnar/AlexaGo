a5914cdc6662eff632fcc7a0a595466643376237
adragnar <robert.adragna1381@gmail.com>
2018-02-04 09:45:28 -0500

    Update MeMegaPiDCMotorTest.ino

--- MeMegaPiDCMotorTest.ino
+++ MeMegaPiDCMotorTest.ino
@@ -15,6 +15,13 @@ int card_length_to_ms(float card_length) {
   return 625*card_length;
 }
 
+int process_input_time (int amount_for) {
+  if (amount_for < 0) {
+        amount_for = -1*amount_for;
+  }
+  return amount_for;
+}
+
 void setup()
 {
   Serial.begin(9600);
@@ -26,128 +33,65 @@ void loop()
 {
     val = Serial.read();
 
-    if (val== 'y') //y is for one dimensional, 1-d motion for/back
-      amount_for = Serial.read(); 
-      if (amount_for < 0) {
-        amount_for = -amount_for;
+    if (val== 'y') { //y is for one dimensional, 1-d motion for/back
+      int amount_for = Serial.read(); 
+      amount_for = process_input_time(amount_for);
       motor2.run(motorSpeed);
       motor1.run(-motorSpeed);
       delay(amount_for); 
       motor2.stop();
       motor1.stop(); 
-    
-      if (val == 'i') {
+    }
+
+    if (val == 'i') {
       motor3.run(motorSpeed);
       delay(2000);
       motor3.stop();
       motor4.run(-motorSpeed);
-      delay(1500)
+      delay(1500);
       motor4.stop();
     }
 
-      if (val == 't') {
-        align();
-        motor3.run(-motorSpeed);
-        delay(1200);
-        motor3.stop();
-
-        motor2.run(motorSpeed);
-        motor1.run(-motorSpeed);
-        delay(1000);
-        motor2.stop();
-        motor1.stop();
-
-        motor4.run(motorSpeed);
-        delay(500);
-        motor4.stop();
-
-        motor3.run(motorSpeed);
-        delay(1000);
-        motor3.stop();
-      
-//      motor4.stop();
-//      motor3.run(-motorSpeed);
-//      delay(500);
-//      motor3.stop();
-//      motor4.run(motorSpeed);
-//      delay(1000);      
-//      motor4.stop();
-//      motor3.run(motorSpeed);
-//      delay(1000);
-//      motor3.stop();
-      
-    }
-
-    else if (val == 'l') {
-      amount_for = Serial.read(); 
-      if (amount_for < 0) {
-        amount_for = -amount_for;
-      motor2.run(-motorSpeed);
-      motor1.run(-motorSpeed);
-      delay(amount_for);
-      motor2.stop();
-      motor1.stop();
-    }
-    else if (val == 'r') {
-      amount_for = Serial.read(); 
-      if (amount_for < 0) {
-        amount_for = -amount_for;
-      motor2.run(motorSpeed);
-      motor1.run(motorSpeed);
-      delay(amount_for);
-      motor2.stop();
-      motor1.stop();
-    }
-
-
-
-
-
-    
-    if (val == 'w') {
-      motor2.run(motorSpeed);
-      motor1.run(-motorSpeed);
-      delay(1000);
-      motor2.stop();
-      motor1.stop();
-    }
-    else if (val == 's') {
-      motor2.run(-motorSpeed);
-      motor1.run(motorSpeed);
-      delay(1000);
-      motor2.stop();
-      motor1.stop();
-    }
-
-    else if (val == 'q') {
+    if (val == 't') {
+      //align();
       motor3.run(-motorSpeed);
-      delay(500);
+      delay(1200);
       motor3.stop();
-      motor4.run(-200);
-      delay(5000);
+
+      motor2.run(motorSpeed);
+      motor1.run(-motorSpeed);
+      delay(1000);
+      motor2.stop();
+      motor1.stop();
+
+      motor4.run(motorSpeed);
+      delay(500);
       motor4.stop();
-    }
-    else if (val == 'e') {
+
       motor3.run(motorSpeed);
-      delay(500);
-      motor3.stop();
-      motor4.run(200);
-      delay(5000);
-      motor4.stop();
-    }  
-
-    else if (val == 'p') { 
-      motor2.run(motorSpeed);
-      motor1.run(-motorSpeed);
       delay(1000);
+      motor3.stop();
+    }
+    
+    if (val == 'l') {
+      int amount_for = Serial.read(); 
+      amount_for = process_input_time(amount_for);
+      motor2.run(-motorSpeed);
+      motor1.run(-motorSpeed);
+      delay(amount_for);
       motor2.stop();
       motor1.stop();
-      motor3.run(-motorSpeed);
-      //delay(card_length_to_ms())
-      
     }
 
-}
+    if (val == 'r') {
+      int amount_for = Serial.read(); 
+      amount_for = process_input_time(amount_for);
+      motor2.run(motorSpeed);
+      motor1.run(motorSpeed);
+      delay(amount_for);
+      motor2.stop();
+      motor1.stop();
+    }
 
 void align()
 {
@@ -159,3 +103,5 @@ void align()
   }
 }
 
+
+}
